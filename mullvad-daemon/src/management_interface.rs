cf54abd2fea3def28f76e7dc6e91a6217e71cc7c
David Lönnhager <david.l@mullvad.net>
2021-04-26 15:01:53 +0200

    Set single exit peer instead of a list of peers

--- mullvad-daemon/src/management_interface.rs
+++ mullvad-daemon/src/management_interface.rs
@@ -887,7 +887,7 @@ fn convert_relay_settings_update(
                             endpoint,
                             protocol: convert_proto_transport_protocol(peer.protocol)?,
                         },
-                        additional_peers: vec![],
+                        exit_peer: None,
                         ipv4_gateway,
                         ipv6_gateway,
                     })
